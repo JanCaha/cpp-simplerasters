5a8fdee06ff13e21be6e173b468a8c8ed1b39be1
Jan Caha <jan.caha@outlook.com>
2025-05-27 13:47:04 +0200

    tests

---
+++ conda/test_script.bat
@@ -0,0 +1,24 @@
+@echo off
+
+echo Running Windows test...
+
+setlocal EnableDelayedExpansion
+
+REM Define a function to check file existence
+:check_file_exists
+set FILE=%1
+if exist "!FILE!" (
+    echo ✅ File exists: !FILE!
+) else (
+    echo ❌ Missing file: !FILE!
+    exit /b 1
+)
+goto :eof
+
+REM Use the function to test required files
+
+call :check_file_exists "%PREFIX%\Library\bin\libsimplerasters.dll"
+call :check_file_exists "%PREFIX%\Library\bin\libsimplerasters.lib"
+
+REM Add more checks as needed
+endlocal
