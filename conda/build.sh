d4d763950855e9b56d8af81553671cfb2e5c4bfe
Jan Caha <jan.caha@outlook.com>
2025-05-27 13:22:18 +0200

    fix vars

--- conda/build.sh
+++ conda/build.sh
@@ -1,3 +1,3 @@
-cmake -B build -S {$SRC_DIR} -G Ninja -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS:bool=off -DPACK_DEB:bool=off
+cmake -B build -S $SRC_DIR -G Ninja -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS:bool=off -DPACK_DEB:bool=off
 cmake --build build --config Release
-cmake --install build --prefix {$PREFIX}
+cmake --install build --prefix $PREFIX
