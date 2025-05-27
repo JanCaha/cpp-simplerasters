687d1c771ed4ae6192b7c7fd0d7094ffafe4a0d1
Jan Caha <jan.caha@outlook.com>
2025-05-27 14:31:05 +0200

    update build

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,6 +1,7 @@
 @echo on
-cmake -B build -S %SRC_DIR% -G Ninja %CMAKE_ARGS% -DBUILD_TESTS:bool=off -DPACK_DEB:bool=off
+
+cmake -B build -S %SRC_DIR% -G Ninja -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS:bool=off -DPACK_DEB:bool=off
 
 cmake --build build --config Release
 
-cmake --install build --prefix %PREFIX%
+cmake --install build --prefix %LIBRARY_PREFIX%
