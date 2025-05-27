e3e2ba96dceda986fb8dac0101d47248eb8bd886
Jan Caha <jan.caha@outlook.com>
2025-05-27 16:17:17 +0200

    args

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,6 +1,6 @@
 @echo on
 
-cmake -B build -S %SRC_DIR% -G Ninja -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS:bool=off -DPACK_DEB:bool=off
+cmake -B build -S %SRC_DIR% -G Ninja %CMAKE_ARGS% -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS:bool=off -DPACK_DEB:bool=off
 
 cmake --build build --config Release
 
