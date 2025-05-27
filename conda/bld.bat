d27cdfb2d6dc1594d763e0135899c1ed6da347a7
Jan Caha <jan.caha@outlook.com>
2025-05-27 14:20:19 +0200

    update

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,7 +1,6 @@
 @echo on
 cmake -B build -S %SRC_DIR% -G Ninja %CMAKE_ARGS% -DBUILD_TESTS:bool=off -DPACK_DEB:bool=off
-if errorlevel 1 exit 1
+
 cmake --build build --config Release
-if errorlevel 1 exit 1
+
 cmake --install build --prefix %PREFIX%
-if errorlevel 1 exit 1
