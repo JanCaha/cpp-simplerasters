ff3aa9f6b96d722805680152406758997fb55584
Jan Caha <jan.caha@outlook.com>
2025-05-27 16:34:58 +0200

    update

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,6 +1,9 @@
 @echo on
 
-cmake -B build -S %SRC_DIR% -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS:bool=off -DPACK_DEB:bool=off
+mkdir build
+cd build
+
+cmake .. -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS:bool=off -DPACK_DEB:bool=off
 
 ninja
 ninja install
