35e5f6096b2c94b2628f76374f6db087ecd43d8e
Jan Caha <jan.caha@outlook.com>
2025-05-29 18:57:21 +0200

    update

--- conda/test_script.bat
+++ conda/test_script.bat
@@ -2,24 +2,7 @@
 
 echo Running Windows test...
 
-setlocal EnableDelayedExpansion
-
-REM Define a function to check file existence
-:check_file_exists
-set FILE=%1
-if exist "!FILE!" (
-    echo ✅ File exists: !FILE!
-) else (
-    echo ❌ Missing file: !FILE!
-    exit /b 1
-)
-goto :eof
-
-REM Use the function to test required files
-
-call :check_file_exists "%PREFIX%\Library\bin\simplerasters.dll"
-call :check_file_exists "%PREFIX%\Library\lib\simplerasters_static.lib"
-call :check_file_exists "%PREFIX%\Library\lib/cmake/SimpleRasters/SimpleRastersConfig.cmake"
-call :check_file_exists "%PREFIX%\Library\include/SimpleRasters/simplerasters.h"
-
-endlocal
+if not exist "%PREFIX%/Library/bin/simplerasters.dll" exit 1
+if not exist "%PREFIX%/Library/lib\simplerasters_static.lib" exit 1
+if not exist "%PREFIX%/Library/lib/cmake/SimpleRasters/SimpleRastersConfig.cmake" exit 1
+if not exist "%PREFIX%/Library/include/SimpleRasters/simplerasters.h" exit 1
