11534ca0c413df598b91f9810b766b90dea6ef27
Jan Caha <jan.caha@outlook.com>
2025-05-31 23:20:15 +0200

    fix name

--- src/api/simplerasters.h
+++ src/api/simplerasters.h
@@ -16,7 +16,7 @@ Copyright (C) 2023 Jan Caha
 #include "ogr_geometry.h"
 
 #if defined( _WIN32 ) || defined( __CYGWIN__ )
-#ifdef VIEWSHED_EXPORTS
+#ifdef SIMPLERASTERS_EXPORTS
 #define DLL_API __declspec( dllexport )
 #else
 #define DLL_API __declspec( dllimport )
