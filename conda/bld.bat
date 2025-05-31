622fafe8788ac59dbf1a61f4676f5ffc3e02f102
Jan Caha <jan.caha@outlook.com>
2025-05-31 12:01:27 +0200

    only build shared

--- conda/bld.bat
+++ conda/bld.bat
@@ -3,7 +3,7 @@
 mkdir build
 
 cmake -B build -S %SRC_DIR% -G "Ninja" -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS:bool=off -DPACK_DEB:bool=off
-cmake --build build --config Release
+cmake --build build --config Release --target simplerasters_shared
 
 REM List build output directory
 dir build
