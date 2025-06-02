e7ebe75cd74af31f59851d87ff6874f04f44aa3c
Jan Caha <jan.caha@outlook.com>
2025-06-02 09:57:40 +0200

    change DLL_API usage

--- src/api/simplerasters.h
+++ src/api/simplerasters.h
@@ -29,46 +29,46 @@ Copyright (C) 2023 Jan Caha
 // Classes
 //////////
 
-class DLL_API AbstractRaster
+class AbstractRaster
 {
   public:
-    bool isValid() const;
+    DLL_API bool isValid() const;
 
-    int xSize() const;
-    int ySize() const;
+    DLL_API int xSize() const;
+    DLL_API int ySize() const;
 
-    int height() const { return ySize(); };
+    DLL_API int height() const { return ySize(); };
 
-    int width() const { return xSize(); };
+    DLL_API int width() const { return xSize(); };
 
-    std::size_t cellsInBand() const;
+    DLL_API std::size_t cellsInBand() const;
 
-    double xCellSize() const;
-    double yCellSize() const;
-    bool hasSquareCells() const;
+    DLL_API double xCellSize() const;
+    DLL_API double yCellSize() const;
+    DLL_API bool hasSquareCells() const;
 
-    void transformCoordinatesToRaster( const double &x, const double &y, double &row, double &col );
-    void transformCoordinatesToWorld( const double &row, const double &col, double &x, double &y );
+    DLL_API void transformCoordinatesToRaster( const double &x, const double &y, double &row, double &col );
+    DLL_API void transformCoordinatesToWorld( const double &row, const double &col, double &x, double &y );
 
-    void transformCoordinatesToRaster( const std::shared_ptr<OGRPoint> p, double &row, double &col );
-    void transformCoordinatesToWorld( const std::shared_ptr<OGRPoint> p, double &x, double &y );
+    DLL_API void transformCoordinatesToRaster( const std::shared_ptr<OGRPoint> p, double &row, double &col );
+    DLL_API void transformCoordinatesToWorld( const std::shared_ptr<OGRPoint> p, double &x, double &y );
 
-    bool isInside( const std::shared_ptr<OGRPoint> p ) const;
-    bool isInside( const OGRPoint p ) const;
+    DLL_API bool isInside( const std::shared_ptr<OGRPoint> p ) const;
+    DLL_API bool isInside( const OGRPoint p ) const;
 
-    bool isProjected() const;
+    DLL_API bool isProjected() const;
 
-    std::string error() const;
+    DLL_API std::string error() const;
 
-    OGRSpatialReference crs() { return mCrs; }
+    DLL_API OGRSpatialReference crs() { return mCrs; }
 
-    OGRPolygon boundingBox();
+    DLL_API OGRPolygon boundingBox();
 
-    OGRPolygon extent() { return boundingBox(); };
+    DLL_API OGRPolygon extent() { return boundingBox(); };
 
-    bool sameDimensions( AbstractRaster &other );
-    bool sameCrs( AbstractRaster &other );
-    bool sameGeotransform( AbstractRaster &other, double epsilon = 4 * std::numeric_limits<double>::epsilon() );
+    DLL_API bool sameDimensions( AbstractRaster &other );
+    DLL_API bool sameCrs( AbstractRaster &other );
+    DLL_API bool sameGeotransform( AbstractRaster &other, double epsilon = 4 * std::numeric_limits<double>::epsilon() );
 
   protected:
     bool mValid;
@@ -91,46 +91,46 @@ class DLL_API AbstractRaster
     void bBoxCoordinates( double &minX, double &maxX, double &minY, double &maxY ) const;
 };
 
-class DLL_API SingleBandRaster : public AbstractRaster
+class SingleBandRaster : public AbstractRaster
 {
   public:
-    SingleBandRaster() {};
-    SingleBandRaster( std::string path, GDALDataType dataType = GDALDataType::GDT_Unknown, int band = 1 );
-    SingleBandRaster( const SingleBandRaster &other, bool copyValues = false );
-    SingleBandRaster( const SingleBandRaster &other, GDALDataType dataType, bool copyValues = false );
+    DLL_API SingleBandRaster() {};
+    DLL_API SingleBandRaster( std::string path, GDALDataType dataType = GDALDataType::GDT_Unknown, int band = 1 );
+    DLL_API SingleBandRaster( const SingleBandRaster &other, bool copyValues = false );
+    DLL_API SingleBandRaster( const SingleBandRaster &other, GDALDataType dataType, bool copyValues = false );
 
-    bool isDataValid() const;
+    DLL_API bool isDataValid() const;
 
-    std::size_t cells() const;
+    DLL_API std::size_t cells() const;
 
-    double value( double row, double column ) const;
-    double value( int row, int column ) const;
-    double value( std::size_t index ) const;
+    DLL_API double value( double row, double column ) const;
+    DLL_API double value( int row, int column ) const;
+    DLL_API double value( std::size_t index ) const;
 
-    double valueAt( double x, double y );
+    DLL_API double valueAt( double x, double y );
 
-    bool isNoData( double row, double column ) const;
-    bool isNoData( int row, int column ) const;
-    bool isNoData( std::size_t index ) const;
+    DLL_API bool isNoData( double row, double column ) const;
+    DLL_API bool isNoData( int row, int column ) const;
+    DLL_API bool isNoData( std::size_t index ) const;
 
-    double noData() const;
-    void setNoData( double value );
+    DLL_API double noData() const;
+    DLL_API void setNoData( double value );
 
-    double cornerValue( double row, double column ) const;
+    DLL_API double cornerValue( double row, double column ) const;
 
-    void writeValue( int row, int column, double value );
-    void writeValue( std::size_t index, double value );
+    DLL_API void writeValue( int row, int column, double value );
+    DLL_API void writeValue( std::size_t index, double value );
 
-    void prefillValues( double value );
+    DLL_API void prefillValues( double value );
 
-    bool saveFile( std::string filename, std::string driverName = "GTiff" );
+    DLL_API bool saveFile( std::string filename, std::string driverName = "GTiff" );
 
-    std::size_t dataSize();
+    DLL_API std::size_t dataSize();
 
-    GDALDataType gdalDataType();
+    DLL_API GDALDataType gdalDataType();
 
-    bool sameDataType( SingleBandRaster &other );
-    bool sameValues( SingleBandRaster &other, double epsilon = 4 * std::numeric_limits<double>::epsilon() );
+    DLL_API bool sameDataType( SingleBandRaster &other );
+    DLL_API bool sameValues( SingleBandRaster &other, double epsilon = 4 * std::numeric_limits<double>::epsilon() );
 
   protected:
     GDALDataType mDataType;
@@ -144,12 +144,13 @@ class DLL_API SingleBandRaster : public AbstractRaster
     std::size_t toIndex( int row, int column ) const;
 };
 
-class DLL_API ProjectedSquareCellRaster : public SingleBandRaster
+class ProjectedSquareCellRaster : public SingleBandRaster
 {
   public:
-    ProjectedSquareCellRaster( std::string path, GDALDataType dataType = GDALDataType::GDT_Unknown, int band = 1 );
-    ProjectedSquareCellRaster( const SingleBandRaster &other, bool copyValues = false );
-    ProjectedSquareCellRaster( const SingleBandRaster &other, GDALDataType dataType, bool copyValues = false );
+    DLL_API ProjectedSquareCellRaster( std::string path, GDALDataType dataType = GDALDataType::GDT_Unknown,
+                                       int band = 1 );
+    DLL_API ProjectedSquareCellRaster( const SingleBandRaster &other, bool copyValues = false );
+    DLL_API ProjectedSquareCellRaster( const SingleBandRaster &other, GDALDataType dataType, bool copyValues = false );
 
   private:
     void validate();
