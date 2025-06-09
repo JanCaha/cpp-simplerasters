ddc7580933619b3d55c883979d4a59412ba1a7ed
Jan Caha <jan.caha@outlook.com>
2025-06-09 09:15:15 +0200

    update script

--- scripts/precommit/clang_format.sh
+++ scripts/precommit/clang_format.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-find src -regex '.*\(cpp\|hpp\|cc\|c\|h\)' -exec clang-format -i {} \;
-find tests -regex '.*\(cpp\|hpp\|cc\|c\|h\)' -exec clang-format -i {} \;
+# Get list of staged files with relevant extensions
+FILES=$(git diff --cached --name-only | grep -E '\.(cpp|hpp|cc|c|h)$')
+
+if [ -z "$FILES" ]; then
+  exit 0
+fi
+
+# Run clang-format on each staged file
+for file in $FILES; do
+  [ -f "$file" ] && clang-format -i "$file"
+done
