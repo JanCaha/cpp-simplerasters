d20bd468aa7d3cc8bfde393e103e235ae4197e7a
Jan Caha <jan.caha@outlook.com>
2026-02-17 12:22:06 +0100

    use preset

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,21 +2,4 @@ BASEDIR=$(dirname "$0")
 
 cd $BASEDIR/..
 
-rm -rf build
-mkdir build
-
-cmake \
-    -S. \
-    -B build \
-    -G Ninja \
-    -D CMAKE_C_COMPILER=clang \
-    -D CMAKE_CXX_COMPILER=clang++ \
-    -D CMAKE_BUILD_TYPE=Release \
-    -D BUILD_TESTS:bool=off \
-    -D PACK_DEB:bool=on \
-    -D CMAKE_INSTALL_PREFIX=/usr
-
-cmake --build build --config Release --target all
-
-sudo cmake --build build --config Release --target install
-sudo cmake --build build --config Release --target pack_deb
+sudo cmake --workflow --preset workflow-release-install
